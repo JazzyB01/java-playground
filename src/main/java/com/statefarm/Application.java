4cba89aae2d0931b14b44ac4fa145c2444e2fc4a
JazzyB01 <82116432+JazzyB01@users.noreply.github.com>
2021-05-08 19:48:36 -0500

    update build.gradle

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -1,8 +1,12 @@
 package com.statefarm;
 
 public class Application {
-
     public static void main(String[] args) {
-        System.out.printf("Hello Jasmine");
+        if (args.length < 1) {
+            System.out.println("Please provide an argument!");
+        } else {
+            System.out.println(args[0]);
+            System.out.println(String.format("Argument provided: %s", args[0]));
+        }
     }
 }
